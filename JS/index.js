586641eba89e8f3dc52ff703ed32be1ce24ffdf6
all4nn <88869328+all4nn@users.noreply.github.com>
2023-05-26 11:21:59 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+let duvida;
+
+/*alert("Você ta lascado!");
+duvida = prompt("Entregue sua prova");
+confirm("Só aceita meus termos e vai dar boa");
+
+document.write("Prova clonada " +duvida);
+*/
+let minha_div = document.querySelector(".container");
+
+minha_div_innerHTML = "<p>Dentro da div</p>"
